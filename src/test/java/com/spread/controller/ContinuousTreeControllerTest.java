24bfb64fd8e26b357277aa8a0e341d6cfa511355
fbielejec <fbielejec@gmail.com>
2017-01-26 11:42:37 +0100

    travis hooks

--- src/test/java/com/spread/controller/ContinuousTreeControllerTest.java
+++ src/test/java/com/spread/controller/ContinuousTreeControllerTest.java
@@ -24,7 +24,7 @@ import com.spread.model.storage.StorageService;
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-public class ContinuousTreeControllerTests {
+public class ContinuousTreeControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
