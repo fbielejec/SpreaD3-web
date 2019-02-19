6a26c910ff372d3a82789b17b1ca45cc85ebd16f
fbielejec <fbielejec@gmail.com>
2019-02-19 21:01:20 +0100

    WIP : test

--- src/test/java/com/spread/AppTests.java
+++ src/test/java/com/spread/AppTests.java
@@ -1,21 +1,38 @@
 package com.spread;
 
+import com.spread.services.visualization.VisualizationService;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class,
                 properties = {
-                    "spring.profiles.active=test",
-                    "log.file.path=/logs",
-                    "storage.location=/spread-storage"
+                    // "spread.vis.location=/home/filip/spread-vis",
+                    "storage.location=/home/filip/spread-storage"
                 })
 public class AppTests {
 
+    @Value("${spread.vis.location}")
+    private String visualizationLocation;
+
+    // @Autowired
+    @MockBean
+    private VisualizationService visualizationService;
+
+    // @Bean
+    // @Primary
+    // public VisualizationService visualizationServiceTest() {
+    //     return Mockito.mock(VisualizationService.class);
+    // }
+
     @Test
     public void contextLoads() {
+        // Mockito.doNothing().when(visualizationService).init(visualizationLocation);
     }
 
 }
--- src/test/java/com/spread/controller/ContinuousTreeControllerTests.java
+++ src/test/java/com/spread/controller/ContinuousTreeControllerTests.java
@@ -10,6 +10,9 @@
 // import java.nio.file.Paths;
 // import java.util.Set;
 
+// import com.google.gson.JsonParser;
+// import com.spread.utils.TestUtils;
+
 // import org.junit.Before;
 // import org.junit.Test;
 // import org.junit.runner.RunWith;
@@ -22,128 +25,127 @@
 // import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 // import org.springframework.web.context.WebApplicationContext;
 
-// import com.google.gson.JsonParser;
-// import com.spread.utils.TestUtils;
-
 // @RunWith(SpringRunner.class)
 // @SpringBootTest
 // public class ContinuousTreeControllerTests {
 
-// 	@Autowired
-// 	private WebApplicationContext webContext;
+//         @Autowired
+//         private WebApplicationContext webContext;
 
-// 	private MockMvc mockMvc;
+//         private MockMvc mockMvc;
 
-// 	@Before
-// 	public void setupMockMvc() throws Exception {
-// 		mockMvc = MockMvcBuilders.webAppContextSetup(webContext) //
-// 				.build();
-// 	}
+//         @Before
+//         public void setupMockMvc() throws Exception {
+//                 mockMvc = MockMvcBuilders.webAppContextSetup(webContext) //
+//                                 .build();
+//         }
 
-// 	@Test
-// 	public void testContinuousTreeController() throws UnsupportedEncodingException, Exception {
+//         @Test
+//         public void testContinuousTreeController() throws UnsupportedEncodingException, Exception {
 
-// 		String response = mockMvc.perform(MockMvcRequestBuilders.get("/token")).andReturn().getResponse()
-// 				.getContentAsString();
+//                 String response = mockMvc.perform(MockMvcRequestBuilders.get("/token")).andReturn().getResponse()
+//                                 .getContentAsString();
 
-// 		String token = new JsonParser().parse(response).getAsJsonObject().get("token").getAsString();
+//                 String token = new JsonParser().parse(response).getAsJsonObject().get("token").getAsString();
 
-// 		postContinuousTree(token);
+//                 System.out.println(token);
 
-// 		getAttributes(token);
+//                 // postContinuousTree(token);
 
-// 		getHpdLevels(token);
+//                 // getAttributes(token);
 
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/hpd-level").header("Authorization", "Bearer " + token)
-// 				.param("hpd-level", "1.1")).andExpect(status().isBadRequest());
+//                 // getHpdLevels(token);
 
-// 		String hpdLevel = String.valueOf(Double.valueOf(TestUtils.expectedHpdLevels.iterator().next()) / 100);
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/hpd-level").header("Authorization", "Bearer " + token)
-// 				.param("hpd-level", hpdLevel)).andExpect(status().isOk());
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/hpd-level").header("Authorization", "Bearer " + token)
+//                 //                 .param("hpd-level", "1.1")).andExpect(status().isBadRequest());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/coordinates/y")
-// 				.header("Authorization", "Bearer " + token).param("attribute", TestUtils.yCoordinate))
-// 				.andExpect(status().isOk());
+//                 // String hpdLevel = String.valueOf(Double.valueOf(TestUtils.expectedHpdLevels.iterator().next()) / 100);
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/hpd-level").header("Authorization", "Bearer " + token)
+//                 //                 .param("hpd-level", hpdLevel)).andExpect(status().isOk());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/coordinates/x")
-// 				.header("Authorization", "Bearer " + token).param("attribute", TestUtils.xCoordinate))
-// 				.andExpect(status().isOk());
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/coordinates/y")
+//                 //                 .header("Authorization", "Bearer " + token).param("attribute", TestUtils.yCoordinate))
+//                 //                 .andExpect(status().isOk());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/external-annotations")
-// 				.header("Authorization", "Bearer " + token).param("has-external-annotations", "true"))
-// 				.andExpect(status().isOk());
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/coordinates/x")
+//                 //                 .header("Authorization", "Bearer " + token).param("attribute", TestUtils.xCoordinate))
+//                 //                 .andExpect(status().isOk());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/timescale-multiplier")
-// 				.header("Authorization", "Bearer " + token).param("timescale-multiplier", "-1.0"))
-// 				.andExpect(status().isBadRequest());
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/external-annotations")
+//                 //                 .header("Authorization", "Bearer " + token).param("has-external-annotations", "true"))
+//                 //                 .andExpect(status().isOk());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/timescale-multiplier")
-// 				.header("Authorization", "Bearer " + token).param("timescale-multiplier", "1.0"))
-// 				.andExpect(status().isOk());
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/timescale-multiplier")
+//                 //                 .header("Authorization", "Bearer " + token).param("timescale-multiplier", "-1.0"))
+//                 //                 .andExpect(status().isBadRequest());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/mrsd").header("Authorization", "Bearer " + token)
-// 				.param("mrsd", "2017/04/06")).andExpect(status().isBadRequest());
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/timescale-multiplier")
+//                 //                 .header("Authorization", "Bearer " + token).param("timescale-multiplier", "1.0"))
+//                 //                 .andExpect(status().isOk());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.post("/continuous/mrsd").header("Authorization", "Bearer " + token)
-// 				.param("mrsd", "2017-04-06")).andExpect(status().isOk());
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/mrsd").header("Authorization", "Bearer " + token)
+//                 //                 .param("mrsd", "2017/04/06")).andExpect(status().isBadRequest());
 
-// 		uploadGeoJson(token);
+//                 // mockMvc.perform(MockMvcRequestBuilders.post("/continuous/mrsd").header("Authorization", "Bearer " + token)
+//                 //                 .param("mrsd", "2017-04-06")).andExpect(status().isOk());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.get("/continuous/output").header("Authorization", "Bearer " + token))
-// 				.andExpect(status().isOk());
+//                 // uploadGeoJson(token);
 
-// 		// TODO: assert json can be retreived from DB by session-id
+//                 // mockMvc.perform(MockMvcRequestBuilders.get("/continuous/output").header("Authorization", "Bearer " + token))
+//                 //                 .andExpect(status().isOk());
 
-// 	}
+//                 // TODO: assert json can be retreived from DB by session-id
 
-// 	public void postContinuousTree(String token) throws Exception {
-// 		String filename = "continuous/speciesDiffusion.MCC.tre";
-// 		File treefile = new File(getClass().getClassLoader().getResource(filename).getFile());
+//         }
 
-// 		Path path = Paths.get(treefile.getAbsolutePath());
-// 		String name = "treefile";
-// 		String originalFileName = treefile.getName();
-// 		String contentType = "text/plain";
-// 		byte[] content = Files.readAllBytes(path);
+//         public void postContinuousTree(String token) throws Exception {
+//                 String filename = "continuous/speciesDiffusion.MCC.tre";
+//                 File treefile = new File(getClass().getClassLoader().getResource(filename).getFile());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.fileUpload("/continuous/tree")
-// 				.file(new MockMultipartFile(name, originalFileName, contentType, content))
-// 				.header("Authorization", "Bearer " + token)).andExpect(status().isOk());
-// 	}
+//                 Path path = Paths.get(treefile.getAbsolutePath());
+//                 String name = "treefile";
+//                 String originalFileName = treefile.getName();
+//                 String contentType = "text/plain";
+//                 byte[] content = Files.readAllBytes(path);
 
-// 	public void getAttributes(String token) throws Exception {
-// 		String response = mockMvc
-// 				.perform(
-// 						MockMvcRequestBuilders.get("/continuous/attributes").header("Authorization", "Bearer " + token))
-// 				.andReturn().getResponse().getContentAsString();
+//                 mockMvc.perform(MockMvcRequestBuilders.fileUpload("/continuous/tree")
+//                                 .file(new MockMultipartFile(name, originalFileName, contentType, content))
+//                                 .header("Authorization", "Bearer " + token)).andExpect(status().isOk());
+//         }
 
-// 		Set<String> attributes = TestUtils.jsonArrayToSet(response);
-// 		assertEquals(TestUtils.expectedAttributes, attributes);
-// 	}
+//         public void getAttributes(String token) throws Exception {
+//                 String response = mockMvc
+//                                 .perform(
+//                                                 MockMvcRequestBuilders.get("/continuous/attributes").header("Authorization", "Bearer " + token))
+//                                 .andReturn().getResponse().getContentAsString();
 
-// 	private void getHpdLevels(String token) throws UnsupportedEncodingException, Exception {
-// 		String response = mockMvc
-// 				.perform(
-// 						MockMvcRequestBuilders.get("/continuous/hpd-levels").header("Authorization", "Bearer " + token))
-// 				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+//                 Set<String> attributes = TestUtils.jsonArrayToSet(response);
+//                 assertEquals(TestUtils.expectedAttributes, attributes);
+//         }
 
-// 		Set<String> hpdLevels = TestUtils.jsonArrayToSet(response);
-// 		assertEquals(TestUtils.expectedHpdLevels, hpdLevels);
-// 	}
+//         private void getHpdLevels(String token) throws UnsupportedEncodingException, Exception {
+//                 String response = mockMvc
+//                                 .perform(
+//                                                 MockMvcRequestBuilders.get("/continuous/hpd-levels").header("Authorization", "Bearer " + token))
+//                                 .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
 
-// 	private void uploadGeoJson(String token) throws Exception {
-// 		String filename = "geojson/subregion_Australia_and_New_Zealand_subunits.geojson";
-// 		File geojsonfile = new File(getClass().getClassLoader().getResource(filename).getFile());
+//                 Set<String> hpdLevels = TestUtils.jsonArrayToSet(response);
+//                 assertEquals(TestUtils.expectedHpdLevels, hpdLevels);
+//         }
 
-// 		Path path = Paths.get(geojsonfile.getAbsolutePath());
-// 		String name = "geojsonfile";
-// 		String originalFileName = geojsonfile.getName();
-// 		String contentType = "text/plain";
-// 		byte[] content = Files.readAllBytes(path);
+//         private void uploadGeoJson(String token) throws Exception {
+//                 String filename = "geojson/subregion_Australia_and_New_Zealand_subunits.geojson";
+//                 File geojsonfile = new File(getClass().getClassLoader().getResource(filename).getFile());
 
-// 		mockMvc.perform(MockMvcRequestBuilders.fileUpload("/continuous/geojson")
-// 				.file(new MockMultipartFile(name, originalFileName, contentType, content))
-// 				.header("Authorization", "Bearer " + token)).andExpect(status().isOk());
-// 	}
+//                 Path path = Paths.get(geojsonfile.getAbsolutePath());
+//                 String name = "geojsonfile";
+//                 String originalFileName = geojsonfile.getName();
+//                 String contentType = "text/plain";
+//                 byte[] content = Files.readAllBytes(path);
+
+//                 mockMvc.perform(MockMvcRequestBuilders.fileUpload("/continuous/geojson")
+//                                 .file(new MockMultipartFile(name, originalFileName, contentType, content))
+//                                 .header("Authorization", "Bearer " + token)).andExpect(status().isOk());
+//         }
 
 // }
