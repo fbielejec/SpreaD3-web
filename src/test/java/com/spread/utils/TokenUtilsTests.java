f20b1a943e9bfeee15bdd986afd809876c8e83bd
fbielejec <fbielejec@gmail.com>
2019-02-20 16:41:49 +0100

    unit tests

--- src/test/java/com/spread/utils/TokenUtilsTests.java
+++ src/test/java/com/spread/utils/TokenUtilsTests.java
@@ -1,42 +1,40 @@
-// package com.spread.unit;
+package com.spread.utils;
 
-// import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-// import java.util.Base64;
-// import java.util.UUID;
+import java.util.Base64;
+import java.util.UUID;
 
-// import javax.crypto.SecretKey;
+import javax.crypto.SecretKey;
 
-// import com.spread.utils.TokenUtils;
+import org.junit.Test;
 
-// import org.junit.Test;
+public class TokenUtilsTests {
 
-// public class TokenUtilsTests {
+    @Test
+    public void testEncodingAndDecoding() {
 
-//     @Test
-//     public void testEncodingAndDecoding() {
+        String secret = "quebaHombre";
+        String base64EncodedSecret = TokenUtils.stringToBase64(secret);
 
-//         String secret = "quebaHombre";
-//         String base64EncodedSecret = TokenUtils.stringToBase64(secret);
+        SecretKey key = TokenUtils.base64StringToKey(base64EncodedSecret);
+        String actualBase64EncodedSecret = TokenUtils.keyToBase64String(key);
 
-//         SecretKey key = TokenUtils.base64StringToKey(base64EncodedSecret);
-//         String actualBase64EncodedSecret = TokenUtils.keyToBase64String(key);
+        assertEquals(base64EncodedSecret, actualBase64EncodedSecret);
 
-//         assertEquals(base64EncodedSecret, actualBase64EncodedSecret);
+        String actualSecret = new String(Base64.getDecoder().decode(actualBase64EncodedSecret));
+        assertEquals(secret, actualSecret);
+    }
 
-//         String actualSecret = new String(Base64.getDecoder().decode(actualBase64EncodedSecret));
-//         assertEquals(secret, actualSecret);
-//     }
+    @Test
+    public void testJWTParsing() {
+        String secret = "quebaHombre";
+        String expectedSessionId = UUID.randomUUID().toString();
 
-//     @Test
-//     public void testJWTParsing() {
-//         String secret = "quebaHombre";
-//         String expectedSessionId = UUID.randomUUID().toString();
+        String jwt = TokenUtils.createJWT(secret, expectedSessionId);
+        String sessionId = TokenUtils.parseJWT(jwt, secret).get(TokenUtils.SESSION_ID).toString();
 
-//         String jwt = TokenUtils.createJWT(secret, expectedSessionId);
-//         String sessionId = TokenUtils.parseJWT(jwt, secret).get(TokenUtils.SESSION_ID).toString();
+        assertEquals(expectedSessionId, sessionId);
+    }
 
-//         assertEquals(expectedSessionId, sessionId);
-//     }
-
-// }
+}
