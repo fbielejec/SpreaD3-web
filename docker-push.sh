e6d0884becb3b0218cf090037590e5208bf2498f
fbielejec <fbielejec@gmail.com>
2019-03-09 11:50:54 +0100

    WIP : travis-docker

--- docker-push.sh
+++ docker-push.sh
@@ -7,6 +7,10 @@ IMG=$NAME:$TAG
 # build jar
 mvn clean package -DskipTests
 
+echo "=============================="
+echo "Buidling: " $IMG
+echo "=============================="
+
 # build and tag as latest
 docker build -t $IMG -f docker-builds/spread-server/Dockerfile .
 docker tag $IMG $NAME:latest
@@ -14,5 +18,13 @@ docker tag $IMG $NAME:latest
 # dockerhub login
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+echo "Pushing: " $NAME
+
 # push to dockerhub
 docker push $NAME
+
+echo "=============================="
+echo "DONE"
+echo "=============================="
+
+exit $?
